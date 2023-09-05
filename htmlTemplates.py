c8480921bf8799ccc48025ee7fa08440e1167ca4
Clinnegan-acc <callum.linnegan@accenture.com>
2023-09-05 16:40:14 +1000

    test

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -4,7 +4,7 @@ css = '''
     padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
 }
 .chat-message.user {
-    background-color: #E6E6FA
+    background-color: #2b313e
 }
 .chat-message.bot {
     background-color: #475063
