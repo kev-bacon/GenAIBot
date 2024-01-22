d950a9a70f885af9f288cd4f2e6212bcb8ce42b9
kev-bacon <kevinnguyen6575@hotmail.com>
2024-01-23 09:15:04 +1100

    Kev's working environment - delete conversation button and chat redesign

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -13,28 +13,32 @@ css = '''
     margin-bottom: 1rem;
     display: flex;
     align-items: center;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Shadow effect */
-    transition: all 0.3s ease; /* Smooth transition */
-    font-size: 1rem; /* Adjust font size */
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); 
+    transition: all 0.3s ease; 
+    font-size: 1rem; 
 }
 
 .chat-message.user {
-    background-color: #2b313e;
+    background-color: #2b313e;  
+    color: #ffffff;  
     justify-content: flex-end;
-    flex-direction: row-reverse; /* Align message to the right */
+    flex-direction: row-reverse;
+    text-align: right;
 }
 
 .chat-message.bot {
-    background-color: #475063;
+    background-color: #475063;  
+    color: #f8f9fa; 
     justify-content: flex-start;
+    text-align: left;
 }
 
 .chat-message .avatar {
     flex: 0 0 auto;
-    width: 50px;
-    height: 50px;
+    width: 75px;
+    height: 75px;
     margin: 0.5rem;
-    border-radius: 50%;
+    border-radius: 30%;
     overflow: hidden;
 }
 
@@ -78,10 +82,10 @@ bot_template = '''
 
 user_template = '''
 <div class="chat-message user">
-    <div class="message">{{MSG}}</div>
     <div class="avatar">
         <img src="https://i.ibb.co/CQ50xgd/default.png">
     </div>    
+    <div class="message">{{MSG}}</div>
 </div>
 '''
 
